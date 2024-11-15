3df0e45ae290049a48cb3eb9da344d3b9596e6a5
Dev-Bhushan-Deshmukh <bhushand967.com>
2024-11-16 01:41:38 +0530

    activity state management

--- petcare-alliance/src/components/Activity.jsx
+++ petcare-alliance/src/components/Activity.jsx
@@ -15,9 +15,26 @@ const[timetable,setTimeTable]=useState(
   activityEnd:'',
   daily:'',
   pets:[],
-  activities:{name:'',start:'',end:'',daily:'',expiry:'',note:''}
+  activities:{name:'',start:'',end:'',daily:'',expiry:'',note:'',icon:'morning'}
 })
 
+const[activityList,setActivityList]=useState([]);
+const pushActivity=()=>{
+let tempActivityList=[...activityList];
+tempActivityList.push(timetable);
+console.log(tempActivityList,'xx')
+setActivityList(tempActivityList)
+setTimeTable(
+  {
+  
+  activityStart:'',
+  activityEnd:'',
+  daily:'',
+  pets:[],
+  activities:{name:'',start:'',end:'',daily:'',expiry:'',note:'',icon:'morning'}
+})
+
+}
 const pets = [
   { src: 'https://w0.peakpx.com/wallpaper/143/746/HD-wallpaper-persian-cat-close-up-gray-cat-kitten-fluffy-cat-cats-domestic-cats-pets-persian.jpg', name: 'Whiskers' },
   { src: 'https://media.istockphoto.com/id/93385233/photo/coahuilan-box-turtle.jpg?s=612x612&w=0&k=20&c=a8l2Td8LuzmxBoGSBjMGRVGTyvDtwNVG0ajxa1AwMno=', name: 'Shelly' },
@@ -68,28 +85,29 @@ setTimeTable(prev=>({...prev,pets:pets}))
 <div className='border w-[30%] h-full rounded-md  '>
 
   
-<h1 className='pl-3 font-semibold bg-slate-50 mb-2 rounded-md'>New activity schedule</h1>
-<div className=' w-full flex gap-3 justify-start items-center pl-3'>
+<h1 className='pl-3 font-semibold  mb-2 rounded-md '>New activity schedule</h1>
+<div className=' w-full flex gap-3 p-1 justify-start items-center pl-3  '>
 <label className='w-[20%]'>From</label>
 <input type="date" id="appt" name="activityStart" className='border  rounded-md' onChange={(e)=>setTimeTable(prev=>({...prev,[e.target.name]:e.target.value}))}/>
 
 
 </div>
-<div className=' w-full flex gap-3 justify-start items-center pl-3'>
+<div className=' w-full flex gap-3  p-1 justify-start items-center pl-3  '>
 <label htmlFor=""className='w-[20%]'>To</label>
-<input type="date" id="appt" name="activityEnd"  className='border  rounded-md' onChange={(e)=>setTimeTable(prev=>({...prev,[e.target.name]:e.target.value}))}/>
+<input type="date" id="appt" name="activityEnd" disabled={timetable.daily}   className={`border  rounded-md `} onChange={(e)=>setTimeTable(prev=>({...prev,[e.target.name]:e.target.value}))}/>
 
 
-<input type="checkbox" id="appt" name="daily"  onChange={(e)=>setTimeTable(prev=>({...prev,[e.target.name]:e.target.value}))}/> 
+<input type="checkbox" id="appt" name="daily"  onClick={(e)=>setTimeTable(prev=>({...prev,[e.target.name]:e.target.checked ?e.target.name:null}))}/> 
+
 <label htmlFor="" >Recuring</label>
 </div>
-<div className=' w-full flex gap-3 justify-start items-center pl-3'>
+<div className=' w-full flex gap-3   p-1justify-start items-center pl-3  '>
 <label className='w-[20%]'>Repeat</label>
 <select name="" id=""></select>
 </div>
 
     <h1 className='pl-3 font-semibold bg-slate-50'>Select pet</h1>
-<div className='flex   gap-1 pl-3 justify-start items-center h-[10%]'>
+<div className='flex   gap-1 pl-3 justify-start items-center h-[10%]  '>
   {pets.map(item=><img src={item.src} onClick={addPets} alt="" className={`${timetable.pets.includes(item.src) &&'border-2 border-red-400 '} rounded-full w-[28px] h-[28px]`} />  
 )}
 
@@ -97,7 +115,7 @@ setTimeTable(prev=>({...prev,pets:pets}))
 
 <h1 className='pl-3 font-semibold bg-slate-50 mb-2'>Activity</h1>
 
-<div className=' pl-3 h-[45%]  justify-center flex flex-col gap-5 '>
+<div className=' pl-3 h-[45%]  justify-center flex flex-col gap-5   '>
 <div className='flex gap-3'><label htmlFor="" className='w-[20%]'>Name</label> <input type="text"   className='border w-[50%] rounded-md' name='name' onChange={(e)=>setTimeTable(prev=>({...prev,activities:{...prev.activities,[e.target.name]:e.target.value}}))}/></div>
 <div className='flex gap-3'><label htmlFor=""className='w-[20%]'>Start</label>
 <input type="time" id="appt"  className='border rounded-md' name="start" onChange={(e)=>setTimeTable(prev=>({...prev,activities:{...prev.activities,[e.target.name]:e.target.value}}))}/></div>
@@ -106,9 +124,9 @@ setTimeTable(prev=>({...prev,pets:pets}))
 <input type="time" id="appt" name="end"  className='border rounded-md'  onChange={(e)=>setTimeTable(prev=>({...prev,activities:{...prev.activities,[e.target.name]:e.target.value}}))}/></div>
 <div className='flex gap-3'>
 
-  <label htmlFor=""className='w-[20%]'>Till</label><input className='border rounded-md text-slate-500 text-center' type="date" id="appt" name="expiry"  onChange={(e)=>setTimeTable(prev=>({...prev,[e.target.name]:e.target.value}))}/>
+  <label htmlFor=""className='w-[20%]'>Till</label><input className={`border rounded-md text-slate-500 text-center `} disabled={timetable.activities.daily} type="date" id="appt" name="expiry"  onChange={(e)=>setTimeTable(prev=>({...prev,activities:{...prev.activities,[e.target.name]:e.target.value}}))}/>
 
-<input type="checkbox" name="daily" id="" value={true}  onChange={(e)=>setTimeTable(prev=>({...prev,activities:{...prev.activities,[e.target.name]:e.target.value}}))} />
+<input type="checkbox" name="daily" id="" value={true}  onChange={(e)=>setTimeTable(prev=>({...prev,activities:{...prev.activities,[e.target.name]:e.target.checked ?e.target.name:null}}))} />
 <label>Daily</label>
 </div>
 
@@ -122,7 +140,7 @@ setTimeTable(prev=>({...prev,pets:pets}))
 
 <div className='flex gap-3 items-center h-[6%] pl-3 '>
 
-< button className='w-[45%] h-[95%] bg-blue-600 rounded-md text-slate-50'>Add</button> < button  className='w-[45%] h-[95%] bg-slate-100 border rounded-md text-slate-400'>Clear</button>
+< button className='w-[45%] h-[95%] bg-blue-600 rounded-md text-slate-50' onClick={pushActivity}>Add</button> < button  className='w-[45%] h-[95%] bg-slate-100 border rounded-md text-slate-400'>Clear</button>
 </div>
 
 
@@ -138,7 +156,7 @@ setTimeTable(prev=>({...prev,pets:pets}))
 <div className=' font-semibold  mb-2 rounded-md h-[100%] w-[35%]  flex items-center border'> <label className='w-1/2 grid content-center text-center h-full bg-slate-100 rounded-tl-md rounded-bl-md  ' htmlFor="">Start</label> <div className='w-1/2 h-full text-center   grid content-center'>{timetable.activityStart }</div> </div>
 
 <div className=' font-semibold  mb-2 rounded-md h-[100%] w-[35%]  flex items-center border'> <label className='w-1/2 grid content-center text-center h-full bg-slate-50 rounded-tl-md rounded-bl-md ' htmlFor="">End</label> <div className='w-1/2 h-full text-center  grid content-center'>
-{timetable.activityEnd }</div> </div>
+{timetable.daily ? timetable.daily :timetable.activityEnd }</div> </div>
 
 <div className=' font-semibold  mb-2 rounded-md h-[100%] w-[25%]  flex items-center border'> <label className='w-1/2 grid content-center text-center h-full bg-slate-100 rounded-tl-md rounded-bl-md  ' htmlFor="">Repeat</label> <div className='w-1/2 h-full text-center   grid content-center'>Monthly</div> </div>
 
@@ -151,37 +169,29 @@ setTimeTable(prev=>({...prev,pets:pets}))
 
 <h1 className='pl-3 font-semibold bg-slate-50 mb-2 h-[7%] flex items-center gap-2'><PiListHeart /> Activities </h1>
 <div className=' flex-1 overflow-y-auto pl-3'>
-<div className='bg-white w-[92%] h-[25%] border-l-blue-600 border-l-[5px] rounded-sm shadow-md  mb-2 pl-1 pr-1 flex flex-col '>
+{activityList.map((item)=><div className='bg-white w-[92%] h-[25%] border-l-blue-600 border-l-[5px] rounded-sm shadow-md  mb-2 pl-1 pr-1 flex flex-col '>
 <div className='flex justify-start items-center h-[40%] w-full gap-3 '>
-<RiSunFill color='orange' size={23} />
-<div className=' flex-1 flex justify-between items-center'><label htmlFor="" className='font-regular text-slate-400 text-sm'>{`${timetable.activities.start}AM- ${timetable.activities.end}PM` }</label>
-<label htmlFor="" className='font-semibold text-slate-500 text-md'>{timetable.activities.name}</label>
-<label htmlFor="" className='font-regular text-slate-400 text-sm'>Daily</label></div>
+
+{item?.activities?.start.split(':')[0]>11 ?<TbSunset2  color='orange' size={23} />:(item?.activities?.start.split(':')[0]>=13 && item?.activities?.start.split(':')[0]<=18  ?<RiSunFill color='orange' size={23} />:<IoMoonSharp color='orange' size={23} />)}
+<div className=' flex-1 flex justify-between items-center'><label htmlFor="" className='font-regular text-slate-400 text-sm'>{`${timetable.activities.start}${timetable.activities.start.split(':')[0]>11 ?'PM':'AM'}- ${timetable.activities.end}${timetable.activities.end.split(':')[0]>11 ?'PM':'AM'}` }</label>
+<label htmlFor="" className='font-semibold text-slate-500 text-md'>{item?.activities?.name}</label>
+<label htmlFor="" className='font-regular text-slate-400 text-sm'>{item.activities.daily ?'Daily':item.activities.expiry}</label></div>
 
 </div>
 
 
 
 <div className='flex-1 text-sm text-slate-500'>
-Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet, dignissimos.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet, dignissimos.
-</div>
+{item.activities.note}</div>
 
 
 
 
 
   
-</div>
-<div className='bg-white w-[92%] h-[25%] border-l-blue-600 border-l-[5px] rounded-sm shadow-md  mb-2'>
+</div>)}
 
-<TbSunset2  color='orange' size={23} />
-  
-</div>
-<div className='bg-white w-[92%] h-[25%] border-l-blue-600 border-l-[5px] rounded-sm shadow-md '>
 
-<IoMoonSharp color='orange' size={23} />
-  
-</div>
 
 
 
